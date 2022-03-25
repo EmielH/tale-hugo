e82f0868e6f82f91574c62525441d71354a7649c
Lukacs T. Berki <blog@lberki.net>
2022-03-25 11:11:27 +0000

    Made the default scheme be the OS default.
    Also a few minor tweaks,

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,12 +1,22 @@
 function initDarkMode() {
     let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    // Apparently if the background color is changed here, the previous
-    // color (in the HTML page) doesn't appear even briefly
+    if (!isDark) {
+	toggleDarkMode();
+    }
+
+    // This is needed because without the requestAnimationFrame() call, the change below takes
+    // effect immediately, even on the transition on the initial page load, which is not
+    // desirable.
+    requestAnimationFrame(() => {
+	document.documentElement.style.setProperty("--color-scheme-transition-time", ".4s");
+    });
 }
 
 function toggleDarkMode() {
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
 }
 
-window.addEventListener("load", initDarkMode);
+// Doing this in an onload handler would let the initial color appear for a
+// split second.
+initDarkMode();
